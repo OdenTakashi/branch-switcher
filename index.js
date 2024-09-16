65172866071162d5136da12024caa949d8456ad3
OdenTakashi <odentakashidesu2@gmail.com>
2024-09-16 11:25:06 +0900

    Add feature select branch

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
+import enquirer from "enquirer";
+const { Select } = enquirer;
 
 exec("git branch", (err, stdout) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(stdout);
+
+  const choices = stdout.split(/\n/);
+  choices.pop();
+  const indexOfCurrentBranch = choices.findIndex((choice) =>
+    choice.includes("*"),
+  );
+  choices.splice(indexOfCurrentBranch, 1);
+
+  const prompt = new Select({
+    message: "You wanna switch to ..🏃‍♀️",
+    choices,
+  });
+  prompt
+    .run()
+    .then((answer) => {
+      try {
+        execSync(`git switch ${answer}`);
+      } catch {
+        console.log("Oops! Please check the Error ..🤔");
+      }
+    })
+    .catch((promptError) => {
+      console.error("Error with prompt...Please Select branch 🪵", promptError);
+    });
 });
