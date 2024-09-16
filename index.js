16f85ec66be56dbdd2adce19011a368f5d8fff6f
OdenTakashi <odentakashidesu2@gmail.com>
2024-09-16 18:29:56 +0900

    Refactor

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ import { exec, execSync } from "child_process";
 import enquirer from "enquirer";
 const { Select } = enquirer;
 
+function parseBranchChoices(branches) {
+  const choices = branches.split('\n').filter(branch => branch.trim() !== '');
+  const indexOfCurrentBranch = choices.findIndex(choice => choice.includes('*'));
+  choices.splice(indexOfCurrentBranch, 1);
+
+  return choices
+}
+
 exec("git branch", (err, stdout) => {
   if (err) {
     console.log(err);
     return;
   }
 
-  const choices = stdout.split(/\n/);
-  choices.pop();
-  const indexOfCurrentBranch = choices.findIndex((choice) =>
-    choice.includes("*"),
-  );
-  choices.splice(indexOfCurrentBranch, 1);
+  const choices = parseBranchChoices(stdout)
 
   const prompt = new Select({
     message: "You wanna switch to ..🏃‍♀️",
     choices,
   });
+
   prompt
     .run()
     .then((answer) => {
